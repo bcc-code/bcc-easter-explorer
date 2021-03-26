c218780e12deb1fc4cd81d06ead3dd28270f5ea4
Bogdan Niculae <bogdaniculae@gmail.com>
2021-03-26 18:18:40 +0200

    fix audio controler

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -229,12 +229,38 @@ const task = {
         const playIconContainer = document.getElementById('play-icon');
         const audioPlayerContainer = document.getElementById('audio-container');
         const seekSlider = document.getElementById('seek-slider');
-        const durationContainer = document.getElementById('duration');
-        const currentTimeContainer = document.getElementById('current-time');
-        const audio = document.querySelector('audio');
         let playState = 'play';
 
-        // Duration
+        playIconContainer.addEventListener('click', () => {
+            if (playState === 'play') {
+                audio.play();
+                audioPlayerContainer.classList.add('playing');
+                requestAnimationFrame(whilePlaying);
+                playState = 'pause';
+            } else {
+                audio.pause();
+                audioPlayerContainer.classList.remove('playing');
+                cancelAnimationFrame(raf);
+                playState = 'play';
+            }
+        });
+
+        const showRangeProgress = (rangeInput) => {
+            if (rangeInput === seekSlider) audioPlayerContainer.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+            else audioPlayerContainer.style.setProperty('--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+        }
+
+        seekSlider.addEventListener('input', (e) => {
+            showRangeProgress(e.target);
+        });
+
+        /** Implementation of the functionality of the audio player */
+
+        const audio = document.querySelector('audio');
+        const durationContainer = document.getElementById('duration');
+        const currentTimeContainer = document.getElementById('current-time');
+        let raf = null;
+
         const calculateTime = (secs) => {
             const minutes = Math.floor(secs / 60);
             const seconds = Math.floor(secs % 60);
@@ -246,29 +272,35 @@ const task = {
             durationContainer.textContent = calculateTime(audio.duration);
         }
 
-        if (audio.readyState > 0) {
-            displayDuration();
-        } else {
-            audio.addEventListener('loadedmetadata', () => {
-                displayDuration();
-            });
-        }
-
-        // Seek slider
-
         const setSliderMax = () => {
             seekSlider.max = Math.floor(audio.duration);
+        }
+
+        const displayBufferedAmount = () => {
+            const bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
+            audioPlayerContainer.style.setProperty('--buffered-width', `${(bufferedAmount / seekSlider.max) * 100}%`);
+        }
+
+        const whilePlaying = () => {
+            seekSlider.value = Math.floor(audio.currentTime);
+            currentTimeContainer.textContent = calculateTime(seekSlider.value);
+            audioPlayerContainer.style.setProperty('--seek-before-width', `${seekSlider.value / seekSlider.max * 100}%`);
+            raf = requestAnimationFrame(whilePlaying);
         }
 
         if (audio.readyState > 0) {
             displayDuration();
             setSliderMax();
+            displayBufferedAmount();
         } else {
             audio.addEventListener('loadedmetadata', () => {
                 displayDuration();
                 setSliderMax();
+                displayBufferedAmount();
             });
         }
+
+        audio.addEventListener('progress', displayBufferedAmount);
 
         seekSlider.addEventListener('input', () => {
             currentTimeContainer.textContent = calculateTime(seekSlider.value);
@@ -284,34 +316,6 @@ const task = {
             }
         });
 
-        audio.addEventListener('timeupdate', () => {
-            seekSlider.value = Math.floor(audio.currentTime);
-        });
-
-        // Click
-
-        let rAF = null;
-
-        const whilePlaying = () => {
-            seekSlider.value = Math.floor(audio.currentTime);
-            currentTimeContainer.textContent = calculateTime(seekSlider.value);
-            audioPlayerContainer.style.setProperty('--seek-before-width', `${seekSlider.value / seekSlider.max * 100}%`);
-            rAF = requestAnimationFrame(whilePlaying);
-        }
-
-        playIconContainer.addEventListener('click', () => {
-            if (playState === 'play') {
-                audio.play();
-                audioPlayerContainer.classList.add('playing');
-                requestAnimationFrame(whilePlaying);
-                playState = 'pause';
-            } else {
-                audio.pause();
-                audioPlayerContainer.classList.remove('playing');
-                requestAnimationFrame(whilePlaying);
-                playState = 'play';
-            }
-        });
     },
 }
 const map = {
